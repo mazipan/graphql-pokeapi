9b93d59fc63bb336e2c846192f01467215487dba
Irfan Maulana <maulana.irfan@tokopedia.com>
2020-11-12 22:05:26 +0700

    feat: move to public

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: ['index.html'],
+  purge: ['*.html'],
   theme: {
     extend: {},
   },
